ffe0988cf1390dc15ac1dced3a693e4646a8bce2
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-08-13 12:44:19 +0000

    Deploying to gh-pages from @ StrongestNumber9/dpf_02@7ce24589da3b87eba67af4fd751f9eee0e0501bd 🚀

---
+++ docs/yet_another_branch/doxygen/search/functions_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['orderdataset_0',['orderDataset',['../classcom_1_1teragrep_1_1functions_1_1dpf__02_1_1BatchCollect.html#a00b243598f8845aac78b565b7f10eb8e',1,'com::teragrep::functions::dpf_02::BatchCollect']]],
+  ['orderdatasetbygivencolumns_1',['orderDatasetByGivenColumns',['../classcom_1_1teragrep_1_1functions_1_1dpf__02_1_1SortOperation.html#a45146062afc6803dc2bb44ff0a7c8b3a',1,'com::teragrep::functions::dpf_02::SortOperation']]]
+];
