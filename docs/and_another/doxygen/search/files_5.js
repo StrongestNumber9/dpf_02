3370e58d757db0e94cfb96d2e7d97a91734454d0
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-08-14 06:17:12 +0000

    Deploying to gh-pages from @ StrongestNumber9/dpf_02@7d10c2e5be44f784af26f94888f447f8fde6990b 🚀

---
+++ docs/and_another/doxygen/search/files_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['testaggregationstep_2ejava_0',['TestAggregationStep.java',['../TestAggregationStep_8java.html',1,'']]]
+];
---
+++ docs/and_another/doxygen/search/variables_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['no_5fpreceding_5faggregate_0',['NO_PRECEDING_AGGREGATE',['../enumcom_1_1teragrep_1_1functions_1_1dpf__02_1_1AbstractStep_1_1CommandProperty.html#a03b334e2d37aa1f238bc11c4f0c51620',1,'com::teragrep::functions::dpf_02::AbstractStep::CommandProperty']]],
+  ['numeric_1',['NUMERIC',['../enumcom_1_1teragrep_1_1functions_1_1dpf__02_1_1SortByClause_1_1Type.html#a5a96597a1d0d9f43b7329d2946540752',1,'com::teragrep::functions::dpf_02::SortByClause::Type']]]
+];
