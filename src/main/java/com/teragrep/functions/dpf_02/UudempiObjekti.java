cd740d2f5fe2d90610938a3eac9bfd930c1052d6
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-08-14 09:27:40 +0300

    New thing

---
+++ src/main/java/com/teragrep/functions/dpf_02/UudempiObjekti.java
@@ -0,0 +1,32 @@
+package com.teragrep.functions.dpf_02;
+
+
+/**
+ * @brief You see nothing here at all
+ *
+ * @responsibilities
+ *  - none whatsoever
+ *
+ * @collaborators
+ *  - SortOperation
+ *
+ * @startuml
+ * class UudempiObjekti {
+ *   + jotain()
+ * }
+ *
+ * note right of UudempiObjekti
+ * Responsibilities:
+ *  - irresponsible to the maximum
+ * end note
+ * @enduml
+ */
+public class UudempiObjekti {
+
+    /**
+     * tekee jotain vielä odottamattomampaa
+     */
+    public void jotain() {
+
+    }
+}
